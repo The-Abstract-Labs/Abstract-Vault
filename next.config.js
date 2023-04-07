08f0d3768d51124fd105a3369d903e239f4985b9
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2023-04-08 00:56:39 +0530

    Abstract Base commit

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
-
-module.exports = nextConfig
+const withPWA = require("next-pwa");
+const runtimeCaching = require("next-pwa/cache");
+const nextConfig = withPWA({
+  // next config
+});
+module.exports = nextConfig;
+module.exports = withPWA({
+  dest: "public",
+  register: true,
+  runtimeCaching,
+  skipWaiting: true,
+});
