435cef3f9757c5b2295ec05ea9416984d902897e
sudoshreyansh <shreyanshjain14072002@gmail.com>
2023-04-08 07:55:29 +0530

    add walletconnect connection

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import web3wallet from '../lib/web3wallet'
+import { useEffect } from 'react'
+import { Web3WalletProvider } from '../contexts/Web3Wallet'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Web3WalletProvider>
+      <Component {...pageProps} />
+    </Web3WalletProvider>
+  )
 }
