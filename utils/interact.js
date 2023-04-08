9631f0579d489596f561be91f0172f1230ced8f7
AMANKANOJIYA <aman.kanojiya4203@gmail.com>
2023-04-08 15:49:46 +0530

    commit for conflict

---
+++ utils/interact.js
@@ -0,0 +1,4 @@
+require("dotenv").config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require("@alch/alchemy-web3");
+const web3 = createAlchemyWeb3(alchemyKey);
